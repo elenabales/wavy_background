ad30760120f157f440d288822884970a0290737b
Elena Balestrieri <34879137+elenabales@users.noreply.github.com>
2023-05-09 20:07:02 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,10 @@
+const list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  (ev) => {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("done");
+    }
+  },
+  false
+);
